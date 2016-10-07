982f1484b2b1cde251b594e34bd500cd7e19b58f
marthematicist <hite.32@gmail.com>
2016-10-07 13:44:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,8 +52,7 @@ function draw() {
     var xColor = lerpColor( leftColor , rightColor , xRatio );
     var yColor = lerpColor( topColor , bottomColor , yRatio );
     var rColor = lerpColor( centerColor , outsideColor , dRatio );
-    var drawColor = lerpColor( xColor , yColor , 0.5 );
-    drawColor = lerpColor( drawColor , rColor , 0.5 );
+    var drawColor = lerpColor( centerColor , outsideColor , dRatio );
     var randColor = color( random( 0 , 255) , random( 0 , 255) , random( 0 , 255) );
     var colorWiggle =  random( minColorWiggle , minColorWiggle );
     drawColor = lerpColor( drawColor , randColor , colorWiggle );
