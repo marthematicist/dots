5caa05c8e98658a9d6267297bdea8926a5c3060a
marthematicist <hite.32@gmail.com>
2016-10-07 11:40:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,2 +1,36 @@
 setupGlobalVariables = function() {
+  xRes = windowWidth;
+  yRes = windowHeight;
+  minRes = min( xRes , yRes );
+  maxRes = max( xRes , yRes );
+  
+  topColor = color( 66, 158 ,244 );
+  bottomColor = color( 89 , 249 , 113 );
+  leftColor = color( 89 , 249 , 175 );
+  rightColor = color( 87 , 29 , 247 );
+  
+  minRad = 0.05*minRes;
+  maxRad = 0.15*minRes;
+}
+
+function setup() {
+  createCanvas( xRex , yRes );
+  background( 255 , 255 , 255 );
+}
+
+function draw() {
+  var x = random( 0 , xRes );
+  var y = random( 0 , yRes );
+  var xRatio = x/xRes;
+  var yRatio = y/yRes;
+  
+  var xColor = lerpColor( leftColor , rightColor , xRatio );
+  var yColor = lerpColor( topColor , bottomColor , yRatio );
+  var drawColor = lerpColor( xColor , yColor , 0.5 );
+  
+  var r = random( minRad , maxRad );
+  
+  fill( drawColor );
+  
+  ellipse( x , y , r );
 }
