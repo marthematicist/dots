b62e04473ee1123e71ac7fdc9e3f0d9b00ad6002
marthematicist <hite.32@gmail.com>
2016-10-10 13:08:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ setupGlobalVariables = function() {
 function setup() {
   setupGlobalVariables();
   createCanvas( xRes , yRes );
-  background( 0 , 0 , 0 );
+  background( 0 , 0 , 0 , 1 );
 }
 
 function draw() {
