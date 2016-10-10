3a16baa0a02b6c72e4816e0037e2ac7356a12f70
marthematicist <hite.32@gmail.com>
2016-10-10 13:13:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ setupGlobalVariables = function() {
   minRad = 0.01*minRes;
   sdRad = 0.05*minRes;
   dotsPerFrame = 5;
+  msPerScene = 15000;
+  startTime = 0 ;
 }
 
 function setup() {
@@ -24,28 +26,34 @@ function setup() {
 }
 
 function draw() {
-  for( var i = 0 ; i < dotsPerFrame ; i++ ) {
-    var x = random( 0 , xRes );
-    var y = random( 0 , yRes );
-    var dx = abs( x - 0.5*xRes );
-    var dy = abs( y - 0.5*yRes );
-    var d = sqrt( dx*dx + dy*dy );
-    var dRatio =  d*d / (maxD*maxD);
-    //dRatio *= dRatio;
-    var rColor = lerpColor( centerColor , outsideColor , dRatio );
-    var drawColor = lerpColor( centerColor , outsideColor , dRatio );
-    var randColor = color( random( 0 , 255) , random( 0 , 255) , random( 0 , 255) );
-    var colorWiggle =  random( minColorWiggle , minColorWiggle );
-    drawColor = lerpColor( drawColor , randColor , colorWiggle );
-    var r = minRad + abs( randomGaussian( 0 , sdRad ) );
-    fill( drawColor );
-    ellipse( x , y , r );
+  if( millis() - startTime > msPerScene ) {
+    background( 0 , 0 , 100 , 1 );
+    centerColor = color( random(0,360) , 100 , 100 , fillAlpha );
+    outsideColor = color(  random(0,360) , 100 , 100 , fillAlpha );
+  } else {
+    for( var i = 0 ; i < dotsPerFrame ; i++ ) {
+      var x = random( 0 , xRes );
+      var y = random( 0 , yRes );
+      var dx = abs( x - 0.5*xRes );
+      var dy = abs( y - 0.5*yRes );
+      var d = sqrt( dx*dx + dy*dy );
+      var dRatio =  d*d / (maxD*maxD);
+      //dRatio *= dRatio;
+      var rColor = lerpColor( centerColor , outsideColor , dRatio );
+      var drawColor = lerpColor( centerColor , outsideColor , dRatio );
+      var randColor = color( random( 0 , 255) , random( 0 , 255) , random( 0 , 255) );
+      var colorWiggle =  random( minColorWiggle , minColorWiggle );
+      drawColor = lerpColor( drawColor , randColor , colorWiggle );
+      var r = minRad + abs( randomGaussian( 0 , sdRad ) );
+      fill( drawColor );
+      ellipse( x , y , r );
+    }
   }
-}
 
-function keyTyped() {
-  if( key === 's' ) {
-    saveCanvas( 'canvas' , 'jpg' );
-    console.log("saved");
+  function keyTyped() {
+    if( key === 's' ) {
+      saveCanvas( 'canvas' , 'jpg' );
+      console.log("saved");
+    }
   }
 }
